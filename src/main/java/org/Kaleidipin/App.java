6333f666e7a2bdf299ffab3f464b8ea86db4f610
V <901375@cognizant.com>
2024-01-13 21:02:38 +0530

    Commit from Main Branch

---
+++ src/main/java/org/Kaleidipin/App.java
@@ -0,0 +1,13 @@
+package org.Kaleidipin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
